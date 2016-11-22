2b5d23eee3f5fe2b54044f3b242a12b0d108b0a4
MengyuanZhu <zhumengyuan@outlook.com>
2016-11-22 12:48:34 -0500

    login and register

--- server/server_app.js
+++ server/server_app.js
@@ -19,7 +19,7 @@ var listPokemons = function(db, username,callback) {
   var collection = db.collection(username);
   collection.find({"pokemon":{'$exists': 1}}).toArray(function(err, docs) {
     assert.equal(err, null);
-    callback(docs)    
+    callback(docs)
   });
 }
 
@@ -39,11 +39,55 @@ var updatePokemon = function(db, username, pokemonName, hp) {
   });
 }
 
-function createHash(password) {
-  var salt = generateSalt(SaltLength);
-  var hash = md5(password + salt);
-  return salt + hash;
+var registerUser = function(db, username, password, callback) {
+  var collection = db.collection(username);
+  collection.find({"password":{'$exists': 1}},{_id:0,name:0}).toArray(function(err, docs) {
+    assert.equal(err, null);
+
+    // verify whether the the username has been registered
+    if (docs.length>0) {
+      callback(1);// username does exist
+      return;
+    }
+    else{
+      //mzhu7 is the salt
+      crypto.pbkdf2(password, 'mzhu7', 100, 512, 'sha512', (err, key) => {
+        if (err) throw err;
+        db.createCollection(username);
+        collection = db.collection(username);
+        collection.insert( {"name" : username, "password":key.toString("hex")}, function(err, result) {
+          assert.equal(err, null);
+          console.log(password)
+          callback(0);
+        });
+      })
+    };
+  });
 }
+
+var login = function(db, username, password, callback) {
+  var collection = db.collection(username);
+  collection.find({"password":{'$exists': 1}},{_id:0,name:0}).toArray(function(err, docs) {
+    assert.equal(err, null);
+
+   // verify whether the the username has been registered
+    if (docs.length<1) {
+      callback(1);//Error: user does not exist
+      return;
+    }
+    else{
+      crypto.pbkdf2(password, 'mzhu7', 100, 512, 'sha512', (err, key) => {
+      if (err) throw err;
+      if (key.toString("hex") ==docs[0]["password"])
+        callback(0); //passwrod is correct
+      else
+        callback(1); //password is wrong
+      })
+    };
+  });
+}
+
+
 
 
 //Use connect method to connect to the server
@@ -51,7 +95,8 @@ MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
-  username="mzhu7"
+  username="mzhu6"
+  password="123"
   pokemonName="Pikachu"
   hp=500
   weight=10
@@ -60,23 +105,25 @@ MongoClient.connect(url, function(err, db) {
   attack=100
   defense=100
 
-  //state 
+
+
+  //state
   //0: show list
   //1: insert pokemon
   //2: delete pokemon
   //3: update pokemon
   //4: register
   //5: verification
-  state=4
-  
+  state=5
+
   if (state==0){
     listPokemons(db, username,function(result) {
       console.log(result)
     });
-    
+
   }
   else if (state==1){
-    insertPokemon(db, username, pokemonName, hp, weight, type, height, attack, defense)
+    insertPokemon(db, username, pokemonName, hp, weight, type, height, attack, defense);
   }
   else if (state==2){
     removePokemon(db, username, pokemonName);
@@ -85,21 +132,16 @@ MongoClient.connect(url, function(err, db) {
     updatePokemon(db, username, pokemonName, 10);
   }
   else if (state==4){
-
-
-    crypto.pbkdf2('ilovedatabase', 'mzhu7', 100, 512, 'sha512', (err, key) => {
-      if (err) throw err;
-      console.log(key.toString('hex'));  // 'c5e478d...1469e50'
-    });
-	
+    registerUser(db, username, password, function(result){
+	    console.log(result)} );
   }
-  
+  else if (state==5){
+    login(db, username, password, function(result){
+	    console.log(result)} );
+  }
 
 
 
-  db.close();
+
+
 });
-
-
-
-
